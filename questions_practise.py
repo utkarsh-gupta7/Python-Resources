0dd959a93dd10ba16f7946ed65aaab64a2f604c8
Utkarsh kumar <utkarsh1276kumar@gmail.com>
2024-06-10 23:16:44 +0530

    A list of 100 questions.
    A list of 100 questions to practice python programming concepts.

---
+++ questions_practise.py
@@ -0,0 +1,55 @@
+'''ques1 -sum with pe-defined variables'''
+# num1 = 22
+# num2 = 23
+# print("the sum of numbers is " , num1+num2)
+#                       #with user input
+# num1 = int(input("type the first number"))
+# num2 = int(input("type the second number"))
+# sum = num1 + num2
+# print("the sum of the numbers is ", sum)
+
+'''ques2 python program to print hello world'''
+# print("hello world)"
+
+'''ques3 python program to find the square root of the given number'''
+        # using exponentiation
+# number = int(input("input the no."))
+# sqrt = (number)**(1/2)
+# print(f"the square root of the given number is {sqrt} ")
+#             #using math module
+# import math
+# num = int(input("input the no. "))
+# squareroot = math.sqrt(num)
+# print(f"the squareroot of the given no. is {squareroot}")
+
+'''  ques 4 python program to calculate the area of a triangle'''
+# when base and height are known
+# h = float(input("enter the height of the triangle:"))  #(here float is used as length can be in decimals also.)
+# b = float(input("enter the base of the triangle:"))
+# area = 0.5 * h * b
+# print(f"the area  of the triangle is {area} ")
+# when the lenght of sides are given
+# a = float(input("enter the length of 1st side:  "))
+# b = float(input("enter the length of 2nd side:  "))
+# c = float(input("enter the length of 3rd side:  "))
+# s = (a+b+c)*0.5
+# area = (s*(s-a)*(s-b)*(s-c))**0.5
+# print(f"The are area of the triangle is {area} sq. units")
+
+'''ques5 python program to solve Quadratic equation ax**2 + bx +c = 0 '''
+# here a, b,c are real numbers and a!=0
+# import cmath    # we have used complex math module so the roots will be in the form of complex no. format.
+# a = int(input("enter the coefficient of x^2: "))
+# b = int(input("enter the coefficient of x: "))
+# c = int(input("enter the value of constant : "))
+# d = (b**2) - (4*a*c)
+#
+# root1 = (-b - cmath.sqrt(d))/ 2*a
+# root2 = (-b + cmath.sqrt(d))/ 2*a
+# print("the roots of the equation are", root1,"and", root2)
+
+'''ques6 write a python program to swap two variables'''
+
+
+
+
